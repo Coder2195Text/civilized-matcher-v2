164d7b77f54b0549a3ecd85404cacaffc7c0ea1c
Coder2195 <coder2195mail@gmail.com>
2023-03-27 19:23:32 -0400

    next seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,37 @@
 import NavBar from "@/components/Navbar";
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider>
+      <DefaultSeo
+        title="Matchmaking"
+        additionalMetaTags={[
+          {
+            name: "msapplication-TileColor",
+            content: "#ff9999",
+          },
+          {
+            name: "theme-color",
+            content: "#ff9999",
+          },
+        ]}
+        openGraph={{
+          images: [
+            {
+              url: "https://civilized-matcher.vercel.app/favicon.ico",
+              width: 256,
+              height: 256,
+              type: "image/png",
+            },
+          ],
+          siteName: "Matchmaking",
+        }}
+      />
       <NavBar />
       <Component {...pageProps} />
     </SessionProvider>
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { signIn, useSession } from "next-auth/react";
 import Layout from "@/components/Layout";
 import Router from "next/router";
+import { NextSeo } from "next-seo";
 
 const Home: FC = () => {
   const { data: session, status } = useSession();
@@ -9,13 +10,19 @@ const Home: FC = () => {
   if (typeof window !== "undefined" && status === "unauthenticated")
     Router.push("/");
   return (
-    <Layout>
-      {status == "authenticated" && (
-        <>
-          <h1>Dashboard</h1>
-        </>
-      )}
-    </Layout>
+    <>
+      <NextSeo
+        title="Matchmaking - Dashboard"
+        description="Manage your matchmaking profile and more."
+      />
+      <Layout>
+        {status == "authenticated" && (
+          <>
+            <h1>Dashboard</h1>
+          </>
+        )}
+      </Layout>
+    </>
   );
 };
 
